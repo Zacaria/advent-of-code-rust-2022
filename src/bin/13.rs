ced7ac48419b93e7c270228981e11429b07a7934
Zacaria <>
2023-02-19 19:14:17 +0000

    [UP]: Add Day 13 part 1 add Comparison enum

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -7,6 +7,7 @@ use nom::{
     IResult,
 };
 
+#[derive(PartialEq, Debug)]
 enum Comparison {
     Ordered,
     UnOrdered,
@@ -20,34 +21,34 @@ enum Cell {
 }
 
 impl Cell {
-    fn compare(self: &Self, b: &Cell) -> Option<bool> {
+    fn compare(self: &Self, b: &Cell) -> Comparison {
         match (self, b) {
-            (Cell::Val(v_a), Cell::Val(v_b)) if v_a < v_b => Some(true),
-            (Cell::Val(v_a), Cell::Val(v_b)) if v_a == v_b => None,
-            (Cell::Val(v_a), Cell::Val(v_b)) if v_a > v_b => Some(false),
+            (Cell::Val(v_a), Cell::Val(v_b)) if v_a < v_b => Comparison::Ordered,
+            (Cell::Val(v_a), Cell::Val(v_b)) if v_a == v_b => Comparison::Undetermined,
+            (Cell::Val(v_a), Cell::Val(v_b)) if v_a > v_b => Comparison::UnOrdered,
             (Cell::Val(_), Cell::Val(_)) => unreachable!(),
             (Cell::Cells(c_a), Cell::Cells(c_b)) => {
                 // loop on B elements
                 for (index_b, item_b) in c_b.iter().enumerate() {
                     // if a is shorter than b, then we have the good order
                     let Some(item_a) = c_a.get(index_b) else {
-                        return Some(true);
+                        return Comparison::Ordered;
                     };
 
                     // return result of a and b comparison if there's a result
                     match item_a.compare(item_b) {
-                        Some(comp) => return Some(comp),
-                        None => continue,
+                        Comparison::Undetermined => continue,
+                        any => return any,
                     };
                 }
 
                 // if a is longer than b, then we have not the good order
                 if c_a.len() > c_b.len() {
-                    return Some(false);
+                    return Comparison::UnOrdered;
                 }
 
                 // no order was found
-                None
+                Comparison::Undetermined
             }
             (Cell::Val(v_a), Cell::Cells(_)) => Cell::Cells(vec![Cell::Val(*v_a)]).compare(b),
             (Cell::Cells(_), Cell::Val(v_b)) => self.compare(&Cell::Cells(vec![Cell::Val(*v_b)])),
@@ -94,7 +95,7 @@ pub fn part_one(input: &str) -> Option<usize> {
             .iter()
             .enumerate()
             .filter_map(|(index, (pair_a, pair_b))| {
-                if pair_a.compare(&pair_b).unwrap_or(true) {
+                if let Comparison::Ordered = pair_a.compare(&pair_b) {
                     Some(index + 1)
                 } else {
                     None
@@ -135,12 +136,12 @@ mod tests {
         let a = Cell::Val(2);
         let b = Cell::Val(5);
 
-        assert_eq!(a.compare(&b).unwrap(), true);
+        assert_eq!(a.compare(&b), Comparison::Ordered);
 
         let a = Cell::Val(5);
         let b = Cell::Val(2);
 
-        assert_eq!(a.compare(&b).unwrap(), false);
+        assert_eq!(a.compare(&b), Comparison::UnOrdered);
     }
 
     #[test]
@@ -160,27 +161,31 @@ mod tests {
             Cell::Val(1),
         ]);
 
-        assert_eq!(a.compare(&b).unwrap(), true);
+        assert_eq!(a.compare(&b), Comparison::Ordered);
+        let a = Cell::Cells(vec![Cell::Val(1), Cell::Val(1)]);
+        let b = Cell::Cells(vec![Cell::Val(1), Cell::Val(1)]);
+
+        assert_eq!(a.compare(&b), Comparison::Undetermined);
 
         let a = Cell::Cells(vec![Cell::Val(1), Cell::Val(2)]);
         let b = Cell::Cells(vec![Cell::Val(2), Cell::Val(3)]);
 
-        assert_eq!(a.compare(&b).unwrap(), true);
+        assert_eq!(a.compare(&b), Comparison::Ordered);
 
         let a = Cell::Cells(vec![Cell::Val(5), Cell::Val(2)]);
         let b = Cell::Cells(vec![Cell::Val(2), Cell::Val(3)]);
 
-        assert_eq!(a.compare(&b).unwrap(), false);
+        assert_eq!(a.compare(&b), Comparison::UnOrdered);
 
         let a = Cell::Cells(vec![Cell::Val(1)]);
         let b = Cell::Cells(vec![Cell::Val(2), Cell::Val(3)]);
 
-        assert_eq!(a.compare(&b).unwrap(), true);
+        assert_eq!(a.compare(&b), Comparison::Ordered);
 
         let a = Cell::Cells(vec![Cell::Val(1), Cell::Val(2)]);
         let b = Cell::Cells(vec![Cell::Val(2)]);
 
-        assert_eq!(a.compare(&b).unwrap(), true);
+        assert_eq!(a.compare(&b), Comparison::Ordered);
     }
 
     #[test]
@@ -192,31 +197,31 @@ mod tests {
         ]);
         let b = Cell::Cells(vec![Cell::Cells(vec![Cell::Val(1)]), Cell::Val(4)]);
 
-        assert_eq!(a.compare(&b).unwrap(), true);
+        assert_eq!(a.compare(&b), Comparison::Ordered);
 
         let a = Cell::Val(1);
         let b = Cell::Cells(vec![Cell::Val(2)]);
 
-        assert_eq!(a.compare(&b).unwrap(), true);
+        assert_eq!(a.compare(&b), Comparison::Ordered);
 
         let a = Cell::Cells(vec![Cell::Val(1)]);
         let b = Cell::Val(2);
 
-        assert_eq!(a.compare(&b).unwrap(), true);
+        assert_eq!(a.compare(&b), Comparison::Ordered);
 
         let a = Cell::Cells(vec![Cell::Val(3)]);
         let b = Cell::Val(2);
 
-        assert_eq!(a.compare(&b).unwrap(), false);
+        assert_eq!(a.compare(&b), Comparison::UnOrdered);
 
         let a = Cell::Val(1);
         let b = Cell::Cells(vec![Cell::Val(2), Cell::Val(3)]);
 
-        assert_eq!(a.compare(&b).unwrap(), true);
+        assert_eq!(a.compare(&b), Comparison::Ordered);
 
         let a = Cell::Cells(vec![Cell::Val(1), Cell::Val(2)]);
         let b = Cell::Val(2);
 
-        assert_eq!(a.compare(&b).unwrap(), true);
+        assert_eq!(a.compare(&b), Comparison::Ordered);
     }
 }
